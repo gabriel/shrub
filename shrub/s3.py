a46dc299421cc34e0dc75354bb14d048186c1650
Gabriel Handford <gabe@YELP-GABE.local>
2008-12-13 22:01:21 -0800

    Fetch with retry

--- shrub/s3.py
+++ shrub/s3.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import urllib
 import logging
 
@@ -10,7 +11,24 @@ class S3:
   
   DefaultLocation = 's3.amazonaws.com'
   
-  def list(self, bucket_name, max_keys, prefix, delimiter, marker, cache=60):
+  def _fetch(self, url, retry_count, **kwargs):
+    """Calls urlfetch.fetch with retry count"""
+    try_count = 0
+    while try_count < retry_count:
+      try:
+        try_count += 1
+        response = urlfetch.fetch(url, **kwargs)
+        # Retry on 5xx errors as well as urlfetch exceptions
+        if int(response.status_code) in range(500, 600):
+          continue
+        return response
+      except Exception, error:
+        logging.error('Error(%s): %s' % (try_count, error))
+        if try_count >= retry_count:
+          raise
+  
+  def list(self, bucket_name, max_keys, prefix, delimiter, marker, cache=60, retry_count=3):
+    if retry_count < 0: raise ValueError, "Invalid retry_count < 0"
     
     url_options = { }
     
@@ -22,13 +40,9 @@ class S3:
     url = u'http://%s/%s?%s' % (S3.DefaultLocation, bucket_name, S3Utils.params_to_url(url_options))
     logging.info("URL: %s", url)
     
+    headers = {'Cache-Control':'max-age=%s' % cache}
     try:
-      headers = {'Cache-Control':'max-age=%s' % cache}
-      response = urlfetch.fetch(url, headers=headers)
+      response = self._fetch(url, retry_count, headers=headers)
       return S3BucketResponse(url, int(response.status_code), response.content)
-    except Exception, detail:
-      logging.error('Error: %s' % detail)
-      return S3ErrorResponse(url, 500, str(detail))
-        
-
-    
+    except Exception, error:
+      return S3ErrorResponse(url, 503, str(error))
