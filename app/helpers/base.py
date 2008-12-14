3bac2c7db18fe22b0b3ca13b5a79563d08a56956
Gabriel Handford <gabe@YELP-GABE.local>
2008-12-13 22:00:30 -0800

    Updating version lookup

--- app/helpers/base.py
+++ app/helpers/base.py
@@ -2,7 +2,7 @@ import os
 import simplejson
 
 def current_version(context):
-  return os.environ['CURRENT_VERSION_ID']
+  return os.environ.get('CURRENT_VERSION_ID','Unknown')
 
 def to_json(context, value):
   return simplejson.dumps(value)
